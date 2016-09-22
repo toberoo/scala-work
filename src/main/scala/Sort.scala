02d4aa4c652011f58e9894ec836c79d55c539cf8
Shehroze Khan <toberoo@gmail.com>
2016-09-22 10:54:05 -0400

    Added quick sort algorithms

--- src/main/scala/Sort.scala
+++ src/main/scala/Sort.scala
@@ -6,25 +6,30 @@ object Sort {
     def mergeSort(arry: List[Int]): List[Int] = {
         if (arry.length == 1) return arry
         val (left, right) = arry.splitAt(arry.length/2)
-        return merge(mergeSort(left), mergeSort(right))
+        merge(mergeSort(left), mergeSort(right))
     }
 
     def merge(left: List[Int], right: List[Int]): List[Int] = (left, right) match {
         case(Nil, right) => right
         case(left, Nil) => left
         case (hL::tL, hR::tR) => {
-            if (hL < hR) return hL::merge(tL, right)
-            return hR::merge(left, tR)
+            if (hL < hR) hL::merge(tL, right)
+            else hR::merge(left, tR)
         }
     }
 
     //Quick sort
-    def quickSort(list: List[Int]): List[Int] = {
+    def quickSortHalfPivot(list: List[Int]): List[Int] = {
         if (list.length < 2) return list
         val pivot = list(list.length/2)
-        quickSort(list.filter (_ < pivot)) ++ 
-        quickSort(list.filter (_ == pivot)) ++ 
-        quickSort(list.filter (_ > pivot))
+        quickSort(list.filter (_ < pivot)) ++ quickSort(list.filter (_ == pivot)) ++ quickSort(list.filter (_ > pivot))
+    }
+
+    def quickSortLinearPivot(list: List[Int]): List[Int] = list match {
+        case(Nil) => Nil
+        case(h::t) =>
+            var (left, right) = t.partition(_ < h)
+            quickSort(left) ++ (h::quickSort(right))
     }
 }
 
